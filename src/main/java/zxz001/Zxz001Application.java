a4e437e5098ff3f868fc62cd497ffbcc1e223538
Matt Cadorette <mcadorette@gmail.com>
2018-10-29 12:28:26 -0400

    Initial commit from Atomist

---
+++ src/main/java/zxz001/Zxz001Application.java
@@ -0,0 +1,12 @@
+package zxz001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zxz001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zxz001Application.class, args);
+	}
+}
